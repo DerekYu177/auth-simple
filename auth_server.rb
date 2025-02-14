08fb42083a64af0321b1a509720cfa05d6561de5
Derek Yu <derek.yu@shopify.com>
2025-02-14 00:24:26 -0500

    Copy comments from 2.1 v12, refactor

--- auth_server.rb
+++ auth_server.rb
@@ -38,6 +38,100 @@ PseudoState = Struct.new do
   def to_s = state
 end
 
+module ResourceServer
+  CLIENT_ID = '1'
+
+  # OAuth 2.1 Section 1.2 Protocol Flow (1)
+  # The client requests authorization from the resource owner.
+  # The authorization request can be made directly to the resource owner,
+  # or preferably indirectly via the authorization server as an intermediary.
+  class AuthenticatedController < ActionController::Base
+    before_action :require_authentication, only: :new
+
+    def new
+      render(json: { result: :authenticated })
+    end
+
+    def index; end
+
+    private
+
+    def require_authentication
+      return if current_user.present?
+
+      redirect_to oauth_authorize_path(
+        client_id: CLIENT_ID,
+        code_challenge: 'random',
+        code_challenge_method: 'S256',
+        authenticated: 1,
+        response_type: 'code',
+        redirect_to: '', # ?
+        state: PseudoState.new.to_s
+      )
+    end
+
+    def current_user
+      ResourceServer::CallbackController.access_token
+    end
+  end
+
+  # OAuth 2.1 Section 1.2 Protocol Flow (2)
+  # The client receives an authorization grant,
+  # which is a credential representing the resource owner's authorization,
+  # expressed using one of the authorization grant types
+  # defined in this specification or using an extension grant type.
+  # The authorization grant type depends on the method used by the
+  # client to request authorization and the types
+  # supported by the authorization server.
+  class CallbackController < ActionController::Base
+    mattr_accessor :access_token
+
+    def new
+      raise 'expected grant, did not receive' unless callback_params[:code]
+
+      # OAuth 2.1 Section 1.2 Protocol Flow (3)
+      # The client requests an access token by authenticating
+      # with the authorization server and presenting the authorization grant.
+      response = post_oauth_tokens!(
+        body: {
+          'grant_type' => 'authorization_code',
+          'code' => callback_params[:code],
+          'client_id' => CLIENT_ID
+        }
+      )
+      tokens = JSON.parse(response.body)
+
+      # well, we don't have a storage mechanism
+      # so I'll have to store it as a class variable. Haha.
+      self.class.access_token = tokens['access_token']
+
+      redirect_to(admin_path)
+    end
+
+    private
+
+    def post_oauth_tokens!(body:)
+      # looks like API requests are _not_ easily supported
+      # as it conflicts with internal Shopify tool
+
+      postenv = {
+        'REQUEST_METHOD' => 'POST',
+        'PATH_INFO' => '/oauth/tokens',
+        'CONTENT_TYPE' => 'application/x-www-form-urlencoded',
+        'HTTP_HOST' => 'localhost',
+        'action_dispatch.request.request_parameters' => body
+      }
+
+      _, _, response = Rails.application.call(postenv)
+      response
+    end
+
+    def callback_params
+      params.permit(:code, :state)
+    end
+  end
+end
+
 module AuthorizationServer
   module OAuth
     class AuthorizationController < ActionController::Base
@@ -58,7 +152,7 @@ module AuthorizationServer
         return oauth_error!('invalid authentication') unless params[:authenticated] == '1'
 
         case authorization_params[:client_id]
-        when '1'
+        when ResourceServer::CLIENT_ID
           redirect_to admin_callback_path(code: 'valid-grant', state: authorization_params[:state])
         else
           oauth_error!('unrecognized client')
@@ -86,6 +180,9 @@ module AuthorizationServer
       end
     end
 
+    # OAuth 2.1 Section 1.2 Protocol Flow (4)
+    # The authorization server authenticates the client
+    # and validates the authorization grant, and if valid, issues an access token.
     class TokensController < ActionController::Base
       skip_before_action :verify_authenticity_token
 
@@ -93,7 +190,7 @@ module AuthorizationServer
         case token_params[:grant_type]
         when 'authorization_code'
           # heh.
-          if token_params[:code] == 'valid-grant'
+          if token_params[:code] == 'valid-grant' && token_params[:client_id] == ResourceServer::CLIENT_ID
             render(
               json: {
                 access_token: 'valid-access-token',
@@ -120,91 +217,6 @@ module AuthorizationServer
       def oauth_error!(error_description, message: 'invalid_request')
         redirect_to(admin_path(error: message, error_description:, **token_params))
       end
-    end
-  end
-end
-
-module ResourceServer
-  class AuthenticatedController < ActionController::Base
-    before_action :require_authentication, only: :new
-
-    def new
-      render(json: { result: :authenticated })
-    end
-
-    def index; end
-
-    private
-
-    def require_authentication
-      return if current_user.present?
-
-      redirect_to oauth_authorize_path(
-        client_id: 1,
-        code_challenge: 'random',
-        code_challenge_method: 'S256',
-        authenticated: 1,
-        response_type: 'code',
-        redirect_to: '', # ?
-        state: PseudoState.new.to_s
-      )
-    end
-
-    def current_user
-      ResourceServer::CallbackController.access_token
-    end
-  end
-
-  class CallbackController < ActionController::Base
-    mattr_accessor :access_token
-
-    def new
-      # we have a grant, now we have to exchange it
-      # for an access token
-
-      response = post_oauth_tokens!
-      tokens = JSON.parse(response.body)
-
-      # well, we don't have a storage mechanism
-      # so I'll have to store it as a class variable. Haha.
-      self.class.access_token = tokens['access_token']
-
-      redirect_to(admin_path)
-    end
-
-    private
-
-    def post_oauth_tokens!
-      # looks like API requests are _not_ easily supported
-      # as it conflicts with internal Shopify tool
-
-      postenv = {
-        'REQUEST_METHOD' => 'POST',
-        'SERVER_NAME' => 'localhost',
-        'SERVER_PORT' => '80',
-        'SERVER_PROTOCOL' => 'HTTP/1.0',
-        'QUERY_STRING' => '',
-        'PATH_INFO' => '/oauth/tokens',
-        'CONTENT_TYPE' => 'application/x-www-form-urlencoded',
-        'CONTENT_LENGTH' => '58',
-        'SCRIPT_NAME' => '',
-        'REMOTE_ADDR' => '127.0.0.1',
-        'HTTP_HOST' => 'localhost',
-        'ORIGINAL_FULLPATH' => '/oauth/tokens',
-        'ORIGINAL_SCRIPT_NAME' => '',
-        'action_dispatch.request.request_parameters' => {
-          'grant_type' => 'authorization_code',
-          'code' => callback_params[:code],
-          'client_id' => '1'
-        }
-      }
-
-      _, _, response = Rails.application.call(postenv)
-      response
-    end
-
-    def callback_params
-      params.permit(:code, :state)
     end
   end
 end
--- auth_server_test.rb
+++ auth_server_test.rb
@@ -31,67 +31,67 @@ class AuthServerTest < ActiveSupport::TestCase
     assert_equal 'authenticated', response['result']
   end
 
-  # def test_authorize_returns_success
-  #   get(
-  #     '/oauth/authorize',
-  #     client_id: 1,
-  #     code_challenge: 'random',
-  #     code_challenge_method: 'S256',
-  #     authenticated: 1,
-  #     response_type: 'code'
-  #   )
-  #   query = parse_redirect(last_response)
+  def test_authorize_returns_success
+    get(
+      '/oauth/authorize',
+      client_id: ResourceServer::CLIENT_ID,
+      code_challenge: 'random',
+      code_challenge_method: 'S256',
+      authenticated: 1,
+      response_type: 'code'
+    )
+    query = parse_redirect(last_response)
 
-  #   assert_includes query, 'code'
-  #   assert_equal 'valid-token', query['code']
+    assert_includes query, 'code'
+    assert_equal 'valid-grant', query['code']
 
-  #   uri = URI(last_response['location'])
-  #   assert_equal '/admin/callback', uri.path
-  # end
+    uri = URI(last_response['location'])
+    assert_equal '/admin/callback', uri.path
+  end
 
-  # def test_authorize_error_redirects
-  #   get(
-  #     '/oauth/authorize',
-  #     client_id: 1,
-  #     code_challenge: 'random',
-  #     code_challenge_method: 'S256',
-  #     authenticated: 2,
-  #     response_type: 'code'
-  #   )
-  #   query = parse_redirect(last_response)
+  def test_authorize_error_redirects
+    get(
+      '/oauth/authorize',
+      client_id: ResourceServer::CLIENT_ID,
+      code_challenge: 'random',
+      code_challenge_method: 'S256',
+      authenticated: 2,
+      response_type: 'code'
+    )
+    query = parse_redirect(last_response)
 
-  #   assert_includes query, 'error'
-  #   assert_includes query, 'error_description'
-  # end
+    assert_includes query, 'error'
+    assert_includes query, 'error_description'
+  end
 
-  # def test_tokens_returns_access_token
-  #   # assume we have the token here
-  #   post(
-  #     '/oauth/tokens',
-  #     grant_type: 'authorization_code',
-  #     code: 'valid-grant',
-  #     client_id: 1
-  #   )
+  def test_tokens_returns_access_token
+    # assume we have the token here
+    post(
+      '/oauth/tokens',
+      grant_type: 'authorization_code',
+      code: 'valid-grant',
+      client_id: ResourceServer::CLIENT_ID
+    )
 
-  #   response = JSON.parse(last_response.body)
+    response = JSON.parse(last_response.body)
 
-  #   assert_includes response, 'access_token'
-  #   assert_includes response, 'token_type'
-  # end
+    assert_includes response, 'access_token'
+    assert_includes response, 'token_type'
+  end
 
-  # def test_tokens_error_if_unrecognized
-  #   post(
-  #     '/oauth/tokens',
-  #     grant_type: 'unsupported'
-  #   )
+  def test_tokens_error_if_unrecognized
+    post(
+      '/oauth/tokens',
+      grant_type: 'unsupported'
+    )
 
-  #   query = parse_redirect(last_response)
+    query = parse_redirect(last_response)
 
-  #   assert_includes query, 'error'
-  #   assert_includes query, 'error_description'
+    assert_includes query, 'error'
+    assert_includes query, 'error_description'
 
-  #   assert_equal 'unsupported_grant_type', query['error']
-  # end
+    assert_equal 'unsupported_grant_type', query['error']
+  end
 
   def default_host
     'localhost'
