8cbbb34a06d7879be5b0a6288be4023ed172bbbf
Derek Yu <derek-nis@hotmail.com>
2025-10-08 01:55:52 +0900

    Refactor oauth/tokens specs to use include_context

--- spec/oauth/tokens_spec.rb
+++ spec/oauth/tokens_spec.rb
@@ -3,82 +3,66 @@
 require 'spec_helper'
 
 RSpec.describe('oauth/tokens') do
-  it 'returns an access token' do
-    # pre-populate the "database"
+  shared_context 'fetches access token' do
+    let(:code_verifier) { "code-verifier:#{SecureRandom.hex(10)}" }
+    let(:access_grant) { 'authorization-code-grant' }
 
-    code_verifier = "code-verifier:#{SecureRandom.hex(10)}"
-    grant = 'authorization-code-grant'
+    before do
+      # pre-populate the "database"
+      cache = AuthorizationServer::Storage.instance
+      cache.authorization_code_grants = {}
+      cache.authorization_code_grants[access_grant] = {
+        code_challenge_method: 'S256',
+        code_challenge: Base64.urlsafe_encode64(Digest::SHA2.hexdigest(code_verifier))
+      }
+    end
 
-    cache = AuthorizationServer::Storage.instance
-    cache.authorization_code_grants = {}
-    cache.authorization_code_grants[grant] = {
-      code_challenge_method: 'S256',
-      code_challenge: Base64.urlsafe_encode64(Digest::SHA2.hexdigest(code_verifier))
-    }
+    subject do
+      post(
+        '/oauth/tokens',
+        grant_type: 'authorization_code',
+        code_verifier: code_verifier,
+        code: access_grant,
+        client_id: ResourceServer::Storage::ID
+      )
 
-    post(
-      '/oauth/tokens',
-      grant_type: 'authorization_code',
-      code_verifier: code_verifier,
-      code: grant,
-      client_id: ResourceServer::Storage::ID
-    )
-
-    response = JSON.parse(last_response.body)
-
-    expect(response).to(include('access_token'))
-    expect(response).to(include('token_type'))
+      JSON.parse(last_response.body)
+    end
   end
 
-  it 'when using reference tokens', access_token_validation_type: 'reference' do
-    code_verifier = "code-verifier:#{SecureRandom.hex(10)}"
-    grant = 'authorization-code-grant'
+  context 'returns valid response' do
+    include_context 'fetches access token'
 
-    cache = AuthorizationServer::Storage.instance
-    cache.authorization_code_grants = {}
-    cache.authorization_code_grants[grant] = {
-      code_challenge_method: 'S256',
-      code_challenge: Base64.urlsafe_encode64(Digest::SHA2.hexdigest(code_verifier))
-    }
+    it 'valid attributes' do
+      response = subject
 
-    post(
-      '/oauth/tokens',
-      grant_type: 'authorization_code',
-      code_verifier: code_verifier,
-      code: grant,
-      client_id: ResourceServer::Storage::ID
-    )
+      expect(response).to(include('access_token'))
+      expect(response).to(include('token_type'))
+    end
 
-    response = JSON.parse(last_response.body)
+    it 'when using reference tokens', access_token_validation_type: 'reference' do
+      response = subject
 
-    expect(response['access_token']).to(include('access-token:'))
-  end
+      expect(response['access_token']).to(include('access-token:'))
+      opaque_access_token = response['access_token']
 
-  it 'when using self-encoded tokens', access_token_validation_type: 'self-encoded' do
-    code_verifier = "code-verifier:#{SecureRandom.hex(10)}"
-    grant = 'authorization-code-grant'
+      # introspect the access token manually to ensure that the attributes are correct
+      access_token = ::OAuth::AccessToken.introspect(opaque_access_token)
 
-    cache = AuthorizationServer::Storage.instance
-    cache.authorization_code_grants = {}
-    cache.authorization_code_grants[grant] = {
-      code_challenge_method: 'S256',
-      code_challenge: Base64.urlsafe_encode64(Digest::SHA2.hexdigest(code_verifier))
-    }
+      expect(access_token['active']).to(be_truthy)
+      expect(access_token['username']).to(eq('DerekYu177'))
+      expect(access_token['aud']).to(eq('https://auth-ser.derekyu.com'))
+    end
 
-    post(
-      '/oauth/tokens',
-      grant_type: 'authorization_code',
-      code_verifier: code_verifier,
-      code: grant,
-      client_id: ResourceServer::Storage::ID
-    )
+    it 'when using self-encoded tokens', access_token_validation_type: 'self-encoded' do
+      response = subject
 
-    response = JSON.parse(last_response.body)
+      payload = OAuth::JWT.introspect(response['access_token'])
 
-    payload = OAuth::JWT.introspect(response['access_token'])
-
-    expect(payload['username']).to(eq('DerekYu177'))
-    expect(payload['client_id']).to(eq(ResourceServer::Storage::ID))
+      expect(payload['username']).to(eq('DerekYu177'))
+      expect(payload['client_id']).to(eq(ResourceServer::Storage::ID))
+      expect(payload['aud']).to(eq('https://auth-ser.derekyu.com'))
+    end
   end
 
   it 'returns unrecognized when token grant-type is unrecognized' do
